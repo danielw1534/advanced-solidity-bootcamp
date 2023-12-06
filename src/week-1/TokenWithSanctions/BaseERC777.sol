7ce28f236dd0fe170db55b157b7eec6c8ef43a6d
danielw1534 <39487081+danielw1534@users.noreply.github.com>
2023-12-06 14:59:16 -0600

    remap

--- src/week-1/TokenWithSanctions/BaseERC777.sol
+++ src/week-1/TokenWithSanctions/BaseERC777.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.7.0;
 
-import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.1-solc-0.7/contracts/token/ERC777/ERC777.sol";
-import
-    "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.1-solc-0.7/contracts/token/ERC777/IERC777Sender.sol";
-import
-    "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.1-solc-0.7/contracts/token/ERC777/IERC777Recipient.sol";
-import
-    "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.1-solc-0.7/contracts/introspection/ERC1820Implementer.sol";
-import
-    "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.1-solc-0.7/contracts/introspection/IERC1820Registry.sol";
+import "@openzeppelin/contracts-07/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts-07/token/ERC777/IERC777Sender.sol";
+import "@openzeppelin/contracts-07/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts-07/introspection/ERC1820Implementer.sol";
+import "@openzeppelin/contracts-07/introspection/IERC1820Registry.sol";
 
 contract TestERC777 is ERC777 {
     constructor(uint256 initialSupply, address[] memory defaultOperators) ERC777("Gold", "GLD", defaultOperators) {
