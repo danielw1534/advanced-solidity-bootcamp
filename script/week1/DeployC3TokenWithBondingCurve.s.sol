48bafb1eb81d63b3475c9cf575a1e91370d6fbf1
danielw1534 <39487081+danielw1534@users.noreply.github.com>
2023-12-13 23:44:56 -0600

    fix import

--- script/week1/DeployC3TokenWithBondingCurve.s.sol
+++ script/week1/DeployC3TokenWithBondingCurve.s.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.22;
 import { C3TokenWithBondingCurve } from "../../src/week1/contracts/C3TokenWithBondingCurve.sol";
 
 import "forge-std/Script.sol";
-import { WETHERC1363 } from "../../src/tokens/WethERC1363.sol";
+import { WETHERC1363 } from "../../src/tokens/WETHERC1363.sol";
 import { ERC1363 } from "../../src/tokens/ERC1363.sol";
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 
